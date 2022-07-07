681319002a0f6094ccbf5f0b652bae3455fe263c
Thomas Biffaud <tbiffaud@gmail.com>
2022-07-07 16:16:25 +0200

    Initial commit

---
+++ Assets/Scripts/Network/NetworkDiscoveryManager.cs
@@ -0,0 +1,83 @@
+using Mirror;
+using Mirror.Discovery;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkDiscoveryManager : MonoBehaviour
+{
+    [SerializeField] NetworkDiscovery networkDiscovery;
+    [SerializeField] AnchorModuleScript anchorModule;
+    [SerializeField] private float delayDiscover = 1.0f;
+
+    [SerializeField] ButtonStatusController buttonHost;
+    [SerializeField] ButtonStatusController buttonClient;
+
+    [SerializeField] List<ButtonStatusController> buttonsAnchors;
+
+    private ServerResponse serverResponse;
+
+    IEnumerator DiscoverNetwork;
+
+    private void Start()
+    {
+        DiscoverNetwork = DiscoverNetworkCoroutine();
+        StartCoroutine(DiscoverNetwork);
+    }
+
+    public void StartHost()
+    {
+        NetworkManager.singleton.StartHost();
+        networkDiscovery.AdvertiseServer();
+        StopCoroutine(DiscoverNetwork);
+        buttonHost.SetStatus(false);
+        StartCoroutine(StartAzureCoroutine(true));
+
+    }
+
+    private IEnumerator StartAzureCoroutine(bool activateButtons)
+    {
+        yield return new WaitForSeconds(2f);
+        anchorModule.StartAzureSession();
+        yield return new WaitForSeconds(4f);
+        if (activateButtons)
+        {
+            ActivateAnchorsButton();
+        }
+    }
+
+    public void StartClient()
+    {
+        NetworkManager.singleton.StartClient(serverResponse.uri);
+        StopCoroutine(DiscoverNetwork);
+        buttonClient.SetStatus(false);
+        StartCoroutine(StartAzureCoroutine(false));
+    }
+
+
+    public void OnDiscoveredServer(ServerResponse info)
+    {
+        Debug.Log("Server discovered");
+        serverResponse = info;
+        buttonHost.SetStatus(false);
+        buttonClient.SetStatus(true);
+    }
+
+    private IEnumerator DiscoverNetworkCoroutine()
+    {   
+        while (true)
+        { 
+            Debug.Log("Discovering network...");
+            networkDiscovery.StartDiscovery();
+            yield return new WaitForSeconds(delayDiscover);
+        }
+    }
+
+    private void ActivateAnchorsButton()
+    {
+        foreach (ButtonStatusController button in buttonsAnchors)
+        {
+            button.SetStatus(true);
+        }
+    }
+}
